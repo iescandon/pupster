2a28afb27746425cf8da45ed89a0f1ce77d6a050
Inez Escandon <iescan4@gmail.com>
2020-08-19 21:33:35 -0500

    merge conflict

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
 function About() {
 	return (
 		<div>
-			<h1>About Page</h1>
-			<p></p>
+			<h1>Welcome to Pupster!</h1>
+			<p>
+				An app for dog lovers everywhere! Use our like feature to browse our
+				pups and see if they want to be your friend. Or use our search feature
+				to search for your favorite breeds and look at our library of adorable
+				pups. Have fun and make friends! Woof!
+			</p>
 		</div>
 	);
 }
-
 export default About;
--- src/components/pages/search.js
+++ src/components/pages/search.js
@@ -1,12 +1,26 @@
 import React from 'react';
-
-function Search() {
+function SearchForm(props) {
 	return (
-		<div>
-			<h1>Search Page</h1>
-			<p></p>
-		</div>
+		<form>
+			<div className="form-group">
+				<label htmlFor="search">Search:</label>
+				<input
+					onChange={props.handleInputChange}
+					value={props.search}
+					name="search"
+					type="text"
+					className="form-control"
+					placeholder="Search for Dog Breed"
+					id="search"
+				/>
+				<button
+					onClick={props.handleFormSubmit}
+					className="btn btn-primary mt-3"
+				>
+					Search
+				</button>
+			</div>
+		</form>
 	);
 }
-
-export default Search;
+export default SearchForm;
